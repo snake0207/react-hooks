b843a3e03bdd7807ba9c02cd1eaaeebe483e08ce
snake0207 <snake0207@gmail.com>
2024-08-09 17:25:32 +0900

    add hook useScroll

--- src/app.jsx
+++ src/app.jsx
@@ -4,16 +4,16 @@ import { useConfirm } from "./hooks/useConfirm";
 import { usePreventLeave } from "./hooks/usePreventLeave";
 import { useFadein } from "./hooks/useFadein";
 import { useNetwork } from "./hooks/useNetwork";
+import { useScroll } from "./hooks/useScroll";
 
 export function App() {
-  const callback = (onLine) => {
-    console.log(`Network is `, onLine ? "Online" : "Offline");
-  };
-  const network = useNetwork(callback);
+  const { x, y } = useScroll();
 
   return (
-    <div>
-      <h1>{network ? "Online" : "Offline"}</h1>
+    <div style={{ height: `1000vh` }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
+        Scroll : {y}
+      </h1>
     </div>
   );
 }
